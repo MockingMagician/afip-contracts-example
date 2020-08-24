7dc3ad1a38a172947b2aad71291c81bbd7573ff9
Marc MOREAU <moreau.marc.web@gmail.com>
2020-08-24 16:34:18 +0200

    change helper

--- src/Helper.php
+++ src/Helper.php
@@ -8,17 +8,17 @@ use Afip\NumberGame\Contracts\HelperInterface;
 
 class Helper implements HelperInterface
 {
-    public static function getRandomNumberBetween(int $min, int $max): int
+    public function getRandomNumberBetween(int $min, int $max): int
     {
         return mt_rand($min, $max);
     }
 
-    public static function getInput(?string $toPromptBeforeInput = null): string
+    public function getInput(?string $toPromptBeforeInput = null): string
     {
         return readline($toPromptBeforeInput);
     }
 
-    public static function output(string $toPrompt)
+    public function output(string $toPrompt)
     {
         echo $toPrompt;
     }
