ed6eab46a28c153e3d642a5d91d4259621b266dc
Marc MOREAU <moreau.marc.web@gmail.com>
2020-08-24 18:30:59 +0200

    game continue methods

--- src/Game.php
+++ src/Game.php
@@ -6,6 +6,7 @@ namespace Afip\NumberGame;
 
 use Afip\NumberGame\Contracts\GameInterface;
 use Afip\NumberGame\Contracts\HelperInterface;
+use Afip\NumberGame\Contracts\LevelInterface;
 
 class Game implements GameInterface
 {
@@ -28,13 +29,23 @@ class Game implements GameInterface
     /**
      * @var int
      */
+    private $maxAttempts = 0;
+    /**
+     * @var int
+     */
     private $countAttempts = 0;
+    /**
+     * @var LevelInterface
+     */
+    private $level;
 
-    public function __construct(string $level, int $min, int $max, HelperInterface $helper)
+
+    public function __construct(LevelInterface $level, int $min, int $max, HelperInterface $helper)
     {
         $this->min = $min;
         $this->max = $max;
         $this->helper = $helper;
+        $this->level = $level;
     }
 
     public function start(): void
@@ -46,15 +57,25 @@ class Game implements GameInterface
 
     private function turn()
     {
+        $this->ItIsFinished();
         /**
          * Ici un tour doit se jouer
          */
     }
 
-    private function end()
+    private function end(string $message)
     {
         /**
          * Ici on peut dire au revoir à l'utilisateur
          */
+    }
+
+    private function ItIsFinished(): bool
+    {
+        if ($this->level->getLevel() === LevelInterface::levels[0] /*easy*/) {
+            return false;
+        }
+
+
     }
 }
