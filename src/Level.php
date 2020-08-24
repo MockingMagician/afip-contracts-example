aed121bbe7a51b6f54beb9b72a92ad86bd7b9e27
Marc MOREAU <moreau.marc.web@gmail.com>
2020-08-24 19:15:42 +0200

    change contract for level

--- src/Level.php
+++ src/Level.php
@@ -12,14 +12,38 @@ class Level implements LevelInterface
      * @var string
      */
     private $level;
+    /**
+     * @var int
+     */
+    private $maxAttemptForEasy;
+    /**
+     * @var int
+     */
+    private $maxAttemptForMedium;
+    /**
+     * @var int
+     */
+    private $maxAttemptForHard;
 
-    public function __construct(string $level)
+    public function __construct(int $maxAttemptForEasy, int $maxAttemptForMedium, int $maxAttemptForHard)
     {
-        $this->level = $level;
+        $this->maxAttemptForEasy = $maxAttemptForEasy;
+        $this->maxAttemptForMedium = $maxAttemptForMedium;
+        $this->maxAttemptForHard = $maxAttemptForHard;
     }
 
     public function getLevel(): string
     {
         return $this->level;
+    }
+
+    public function setLevel(string $level): string
+    {
+        // TODO: Implement setLevel() method.
+    }
+
+    public function getMaxAttempts(): string
+    {
+        // TODO: Implement getMaxAttempts() method.
     }
 }
