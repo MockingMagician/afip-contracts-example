ce76a5082f56a37e955c327169d11cd04dc2074b
Marc MOREAU <moreau.marc.web@gmail.com>
2020-08-24 22:05:08 +0200

    fix interfaces

--- src/Contracts/GameInterface.php
+++ src/Contracts/GameInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Afip\NumberGame\Contracts;
 
-
+/**
+ * Interface GameInterface
+ * @package Afip\NumberGame\Contracts
+ *
+ * Just the game
+ */
 interface GameInterface
 {
     /**
