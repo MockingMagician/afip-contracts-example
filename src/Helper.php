8d757f46f8406707aebc8f1fbf3181a513244bf9
wissem38 <63680757+wissem38@users.noreply.github.com>
2020-08-25 14:47:49 +0200

    ajout class helper

---
+++ src/Helper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Afip\NumberGame;
+
+use Afip\NumberGame\Contracts\HelperInterface;
+
+class helper implements HelperInterface
+{
+    public function getRandomNumberBetween(int $min, int $max): int
+    {
+        return random_int($min,$max);
+
+    }
+
+    public function getInput(?string $toPromptBeforeInput): string
+    {
+        return readline($toPromptBeforeInput);
+    }
+
+    public function output(string $toPrompt)
+    {
+        echo $toPrompt;
+
+    }
+    
+}
